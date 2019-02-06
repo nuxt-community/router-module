ed63475ade5527ffe2fb63a2039eeaa2377a78cb
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-02-05 23:35:38 -0200

    fix(module): use `consola` instead of `throw` when router file not found

--- test/fail.test.js
+++ test/fail.test.js
@@ -1,19 +1,26 @@
-const { Nuxt, Builder } = require('nuxt')
+const { Nuxt } = require('nuxt')
+const consola = require('consola')
+
+consola.mockTypes(() => jest.fn())
 
 jest.setTimeout(60000)
 
 const config = require('./fixture/fail/nuxt.config')
 
 describe('Module', () => {
-  test('Build should fail if no router.js', async () => {
+  let nuxt
+
+  beforeAll(async () => {
     config.dev = false
-    const nuxt = new Nuxt(config)
-    await new Builder(nuxt).build()
-    try {
-      await new Builder(nuxt).build()
-    } catch (err) {
-      expect(err.message).toContain('Please create a router.js file in your source folder.')
-    }
+    nuxt = new Nuxt(config)
+  })
+
+  afterAll(async () => {
     await nuxt.close()
+  })
+
+  test('should warn if not found the router file', async () => {
+    expect(consola.warn).toBeCalledTimes(1)
+    expect(consola.warn).toBeCalledWith(expect.stringMatching(/^No `(.*)` file found in `(.*)`.$/))
   })
 })
